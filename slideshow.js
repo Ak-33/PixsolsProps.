944800fa43ec1d3c054191f9b313ffb74ab502f5
Ak-33 <112743875+Ak-33@users.noreply.github.com>
2025-01-22 19:28:27 -0500

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -11,25 +11,18 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var images = document.getElementsByClassName("mySlides16-9");
-  var videos = document.getElementsByClassName("videoSlides");
+  // Combine images and videos into one array
+  var slides = document.querySelectorAll(".mySlides16-9, .videoSlides");
 
   // If n is out of bounds, wrap around
-  if (n > images.length + videos.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = images.length + videos.length }
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
 
-  // Hide all images and videos
-  for (i = 0; i < images.length; i++) {
-    images[i].style.display = "none";
-  }
-  for (i = 0; i < videos.length; i++) {
-    videos[i].style.display = "none";
+  // Hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
 
-  // Display the current image or video
-  if (slideIndex <= images.length) {
-    images[slideIndex - 1].style.display = "block";
-  } else {
-    videos[slideIndex - images.length - 1].style.display = "block";
-  }
+  // Display the current slide
+  slides[slideIndex - 1].style.display = "block";
 }
